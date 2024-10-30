9f842a0185bfdf6175f870af15ba7b2d362dfc25
Malka <b7malka@gmail.com>
2024-10-30 22:04:49 +0200

    Initial code + Welcome functions

---
+++ Stage0/Program1515.cs
@@ -0,0 +1,16 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome1515();
+        welcome8245();
+        Console.ReadKey();
+    }
+    static partial void welcome8245();
+    private static void welcome1515()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
