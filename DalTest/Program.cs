3639d42151924648c654edc277f4d68e5089aae4
Malka <b7malka@gmail.com>
2024-11-25 16:56:12 +0200

    Assignment

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -14,7 +14,7 @@ namespace DalTest
 
         // Define an enum with specific values
 
-        private void volunteerMenu() 
+        private void volunteerMenu()
         {
             try
             {
@@ -39,52 +39,30 @@ namespace DalTest
                             volunteerUpdate();
                             break;
                         case SpecificOptions.DELETE:
-                            printAllData();
+                            volunteerDelete();
                             break;
                         case SpecificOptions.DELETE_ALL:
-                            configMenu();
+                            volunteerDeleteAll();
                             break;
                         default:
                             Console.WriteLine("Invalid choice.");
                             break;
                     }
-                }while (choice!=SpecificOptions.EXIT);
-  
+                } while (choice != SpecificOptions.EXIT);
+
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
 
         private void volunteerUpdate()
         {
-            Console.WriteLine("Enter an ID");
-            string id = Console.ReadLine();
-            
-
-        }
-
-        private void volunteerReadAll()
-        {
-            List<Volunteer> volunteerList = s_dalVolunteer.ReadAll();
-            foreach (var v in volunteerList)
-            {
-                Console.WriteLine(v);
-            }
-        }
-        private void volunteerRead() {
-            Console.WriteLine("Enter an ID");
-            string id=Console.ReadLine();
-            Volunteer volunteer = s_dalVolunteer.Read(int.Parse(id));
-            Console.WriteLine(volunteer);//לבדוק איך כותב למסך
-        }
-
-        private void volunteerCreate()
-        {
             Console.WriteLine("Enter your ID");
             string id = Console.ReadLine();
             Console.WriteLine("Enter your name");
-            string name=Console.ReadLine();
+            string name = Console.ReadLine();
             Console.WriteLine("Enter your phone");
             string phone = Console.ReadLine();
             Console.WriteLine("Enter your email");
@@ -98,12 +76,143 @@ namespace DalTest
             Console.WriteLine("Enter your role");
             string role = Console.ReadLine();
             Console.WriteLine("Enter your type of distance");
-            Role convertedRole=(Role)Enum.Parse(typeof(Role),role);
+            Role convertedRole = (Role)Enum.Parse(typeof(Role), role);
             string typeOfDistance = Console.ReadLine();
-            TypeOfDistance convertedTypeOfDistance = (TypeOfDistance)Enum.Parse(typeof(TypeOfDistance),typeOfDistance);
+            TypeOfDistance convertedTypeOfDistance = (TypeOfDistance)Enum.Parse(typeof(TypeOfDistance), typeOfDistance);
+            s_dalVolunteer.Update(new(int.Parse(id), name, phone, email, password, address, null, null, convertedRole, false, int.Parse(maxDistance), convertedTypeOfDistance));
+        }
+
+        private void volunteerReadAll()
+        {
+            List<Volunteer> volunteerList = s_dalVolunteer.ReadAll();
+            foreach (var v in volunteerList)
+            {
+                Console.WriteLine(v);
+            }
+        }
+        private void volunteerRead()
+        {
+            Console.WriteLine("Enter an ID");
+            string id = Console.ReadLine();
+            Volunteer volunteer = s_dalVolunteer.Read(int.Parse(id));
+            Console.WriteLine(volunteer);//לבדוק איך כותב למסך
+        }
+
+        private void volunteerCreate()
+        {
+            Console.WriteLine("Enter your ID");
+            string id = Console.ReadLine();
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your phone");
+            string phone = Console.ReadLine();
+            Console.WriteLine("Enter your email");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your password");
+            string password = Console.ReadLine();
+            Console.WriteLine("Enter your max distance");
+            string maxDistance = Console.ReadLine();
+            Console.WriteLine("Enter your role");
+            string role = Console.ReadLine();
+            Console.WriteLine("Enter your type of distance");
+            Role convertedRole = (Role)Enum.Parse(typeof(Role), role);
+            string typeOfDistance = Console.ReadLine();
+            TypeOfDistance convertedTypeOfDistance = (TypeOfDistance)Enum.Parse(typeof(TypeOfDistance), typeOfDistance);
             s_dalVolunteer.Create(new(int.Parse(id), name, phone, email, password, address, null, null, convertedRole, false, int.Parse(maxDistance), convertedTypeOfDistance));
         }
-        private void assignmentMenu() 
+        private void volunteerDelete()
+        {
+            Console.WriteLine("Enter your ID");
+            string id = Console.ReadLine();
+            s_dalVolunteer.Delete(int.Parse(id));
+        }
+        private void volunteerDeleteAll()
+        {
+            s_dalVolunteer.DeleteAll();
+        }
+        private void assignmentMenu()
+        {
+            try
+            {
+                SpecificOptions choice = SpecificOptions.EXIT;
+                do
+                {
+                    choice = (SpecificOptions)Enum.Parse(typeof(SpecificOptions), Console.ReadLine());
+                    switch (choice)
+                    {
+                        case SpecificOptions.EXIT:
+                            break;
+                        case SpecificOptions.CREATE:
+                            assignmentCreate();
+                            break;
+                        case SpecificOptions.READ:
+                            assignmentRead();
+                            break;
+                        case SpecificOptions.READ_ALL:
+                            assignmentReadAll();
+                            break;
+                        case SpecificOptions.UPDATE:
+                            assignmentUpdate();
+                            break;
+                        case SpecificOptions.DELETE:
+                            assignmentDelete();
+                            break;
+                        case SpecificOptions.DELETE_ALL:
+                            assignmentDeleteAll();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice.");
+                            break;
+                    }
+                } while (choice != SpecificOptions.EXIT);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+        private void assignmentCreate()
+        {
+            Console.WriteLine("Enter the call id");
+            string callId = Console.ReadLine();
+            Console.WriteLine("Enter the volunteer's id");
+            string volunteerId = Console.ReadLine();
+            s_dalAssignment.Create(new(0, int.Parse(callId), int.Parse(volunteerId), s_dalConfig.Clock, null, null));
+            //newId, item.CallId, item.VolunteerId, item.TreatmentStartTime, item.TreatmentEndTime, item.TypeOfTreatmentEnding
+        }
+
+        private void assignmentRead()
+        {
+            Console.WriteLine("Enter an ID");
+            string id = Console.ReadLine();
+            Console.WriteLine(s_dalAssignment.Read(int.Parse(id)));
+        }
+
+        private void assignmentReadAll()
+        {
+            List<Assignment> assignmentList = s_dalAssignment.ReadAll();
+            foreach (var item in assignmentList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private void assignmentUpdate()
+        {
+            Console.WriteLine("Enter assignment ID");
+            string assignmentId=Console.ReadLine();
+            s_dalAssignment.Read(int.Parse(assignmentId));
+            Console.WriteLine("Enter the call id");
+            string callId = Console.ReadLine();
+            Console.WriteLine("Enter the volunteer's id");
+            string volunteerId = Console.ReadLine();
+            s_dalAssignment.Update(new(int.Parse(assignmentId), int.Parse(callId), int.Parse(volunteerId), s_dalConfig.Clock, null, null));
+        }
+
+        private void assignmentDelete()
         {
 
         }
