88bbec5c1a972f328a06085c9f48541b7a43d020
Batya Bracha Oren <batyabracha2005@gmail.com>
2024-12-04 22:06:49 +0200

    פרק 5 שלב 2

--- DalList/Dal/DalList.cs
+++ DalList/Dal/DalList.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace Dal;
+﻿namespace Dal;
 using DalApi;
 sealed public class DalList : IDal
 {
