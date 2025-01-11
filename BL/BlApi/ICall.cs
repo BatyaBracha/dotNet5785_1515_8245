ef63cdbf279c513716a7a528290aa1eadf7b93f0
Malka <b7malka@gmail.com>
2025-01-11 20:54:13 +0200

    in the middle of icall

--- BL/BlApi/ICall.cs
+++ BL/BlApi/ICall.cs
@@ -5,16 +5,16 @@ public interface ICall
 {
     void Create(BO.Call boCall);
     BO.Call? Read(int id);
-
-    IEnumerable<BO.CallInList> ReadAll(BO.CallFieldSort? sort = null, BO.CallFieldFilter? filter = null, object? value = null);
-    void Update(BO.Student boStudent);
+    public int[] GetCallQuantitiesByStatus(BO.CallInList callInList);
+    IEnumerable<BO.CallInList> ReadAll(BO.CallFieldFilter? sort = null, BO.CallFieldFilter? filter = null, object? value = null);
+    void Update(BO.Call boCall);
     void Delete(int id);
 
     void RegisterStudentToCourse(int studentId, int courseId);
     void UnRegisterStudentFromCourse(int studentId, int courseId);
 
-    IEnumerable<BO.CourseInList> GetRegisteredCoursesForStudent(int studentId, BO.Year year = BO.Year.None);
-    IEnumerable<BO.CourseInList> GetUnRegisteredCoursesForStudent(int studentId, BO.Year year = BO.Year.None);
+    IEnumerable<BO.CallInList> GetRegisteredCoursesForStudent(int studentId, BO.Year year = BO.Year.None);
+    IEnumerable<BO.CallInList> GetUnRegisteredCoursesForStudent(int studentId, BO.Year year = BO.Year.None);
 
     BO.StudentGradeSheet GetGradeSheetPerStudent(int studentId, BO.Year year = BO.Year.None);
     void UpdateGrade(int studentId, int courseId, double grade);
