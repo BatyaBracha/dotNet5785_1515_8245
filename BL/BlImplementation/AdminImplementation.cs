7277baf246954c88520b35d384a46f1da96f57a1
Malka <b7malka@gmail.com>
2025-01-11 22:28:55 +0200

    8b

---
+++ BL/BlImplementation/AdminImplementation.cs
@@ -0,0 +1,39 @@
+﻿
+
+using BlApi;
+
+
+namespace BlImplementation;
+
+internal class AdminImplementation : IAdmin
+{
+    public DateTime Clock()
+    {
+        throw new NotImplementedException();
+    }
+
+    public TimeSpan GetRiskRange()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InitializeDB()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void PromotionClock(BO.TimeUnit timeUnit)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ResetDB()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetRiskRange(TimeSpan riskRange)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BL/BlImplementation/VolunteerImplementation.cs
@@ -0,0 +1,43 @@
+﻿
+
+using BlApi;
+namespace BlImplementation;
+
+internal class VolunteerImplementation : IVolunteer
+{
+    private readonly DalApi.IDal Volunteer_dal = DalApi.Factory.Get;
+    public void Create(BO.Volunteer boVolunteer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MatchVolunteerToCall(int volunteerId, int callId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public BO.Volunteer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<BO.VolunteerInList> ReadAll(BO.Active? sort = null, BO.VolunteerFields? filter = null, object? value = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UnMatchVolunteerToCall(int volunteerId, int callId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(BO.Volunteer boVolunteer)
+    {
+        throw new NotImplementedException();
+    }
+}
