95488c2cc2d5dfac83a5827344b0771ab0a76701
Batsheva-Malka <hand4d30@mbjcomp.org.il>
2025-05-26 01:18:16 +0300

    At the end of 9d.

--- PL/Volunteer/VolunteerWindow.xaml.cs
+++ PL/Volunteer/VolunteerWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,65 @@ namespace PL.Volunteer
     /// </summary>
     public partial class VolunteerWindow : Window
     {
-        public VolunteerWindow()
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+        public static readonly DependencyProperty ButtonTextProperty =
+       DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow), new PropertyMetadata("Add"));
+
+
+
+        public BO.Volunteer? CurrentVolunteer
         {
+            get { return (BO.Volunteer)GetValue(CurrentVolunteerProperty); }
+            set { SetValue(CurrentVolunteerProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentVolunteer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentVolunteerProperty =
+            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
+
+
+        public string ButtonText
+        {
+            get { return (string)GetValue(ButtonTextProperty); }
+            set { SetValue(ButtonTextProperty, value); }
+        }
+        public BO.Role Role { get; set; } = BO.Role.STANDARD;
+        public void comboBoxRole_SelectionChanged(object sender, SelectionChangedEventArgs e)
+              => CurrentVolunteer.Role = sender is ComboBox comboBox ? (BO.Role)comboBox.SelectedItem : BO.Role.STANDARD;
+
+        public BO.TypeOfDistance TypeOfDistance { get; set; } = BO.TypeOfDistance.WALK;
+        public void comboBoxTypeOfDistance_SelectionChanged(object sender, SelectionChangedEventArgs e)
+              => CurrentVolunteer.TypeOfDistance = sender is ComboBox comboBox ? (BO.TypeOfDistance)comboBox.SelectedItem : BO.TypeOfDistance.WALK;
+
+        private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (ButtonText == "Add")
+                {
+                    s_bl.Volunteer.Create(CurrentVolunteer!);
+                    MessageBox.Show("Volunteer added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    s_bl.Volunteer.Update(CurrentVolunteer.Id, CurrentVolunteer!);
+                    MessageBox.Show("Volunteer updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        public VolunteerWindow(int Id=0)
+        {
+            ButtonText = Id == 0 ? "Add" : "Update";
+
             InitializeComponent();
+            CurrentVolunteer = (Id != 0) ? s_bl.Volunteer.Read(Id)! 
+            :new BO.Volunteer(0, "", "", "", "", null, null, null, BO.Role.STANDARD, true, null, BO.TypeOfDistance.WALK, 0, 0, 0, null);
         }
     }
 }
