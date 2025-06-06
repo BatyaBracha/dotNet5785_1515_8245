30e082d18ca273727db58743527456e8a87577f5
BatyaBracha <Hand4d2@mbjcomp.org.il>
2025-06-06 17:14:52 +0300

    finished the converter!!!!

---
+++ PL/Volunteer/ConvertUpdateToTrue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PL.Volunteer
+{
+    public class ConvertUpdateToTrue : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() == "Update";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
