57759273f37fbe06bdf05d895ae8f1b98ff11839
Batsheva-Malka <hand4d30@mbjcomp.org.il>
2025-06-29 19:17:06 +0300

    after fixing

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -1,72 +1,22 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//namespace PL;
-
-//internal class VolunteerFieldsCollection : IEnumerable
-//{
-//    static readonly IEnumerable<BO.VolunteerFields> s_enums =
-//                  (Enum.GetValues(typeof(BO.VolunteerFields)) as IEnumerable<BO.VolunteerFields>)!;
-
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-//internal class CallFieldsCollection : IEnumerable
-//{
-//    static readonly IEnumerable<BO.CallInListField> s_enums =
-//                  (Enum.GetValues(typeof(BO.CallInListField)) as IEnumerable<BO.CallInListField>)!;
-
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-
-//public class TypeOfCallCollection : IEnumerable
-//{
-//    static readonly IEnumerable<BO.TypeOfCall> s_enums =
-//                  (Enum.GetValues(typeof(BO.TypeOfCall)) as IEnumerable<BO.TypeOfCall>)!;
-
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-
-//public class Role : IEnumerable
-//{
-//    static readonly IEnumerable<BO.Role> s_enums =
-//                  (Enum.GetValues(typeof(BO.Role)) as IEnumerable<BO.Role>)!;
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-//public class TypeOfDistance : IEnumerable
-//{
-//    static readonly IEnumerable<BO.TypeOfDistance> s_enums =
-//                  (Enum.GetValues(typeof(BO.TypeOfDistance)) as IEnumerable<BO.TypeOfDistance>)!;
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-//public class CallStatusCollection : IEnumerable
-//{
-//    static readonly IEnumerable<BO.CallStatus> s_enums =
-//                  (Enum.GetValues(typeof(BO.CallStatus)) as IEnumerable<BO.CallStatus>)!;
-
-//    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-//}
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PL;
-
-public class VolunteerFieldsCollection : IEnumerable
+internal class VolunteerFieldsCollection : IEnumerable
 {
     static readonly IEnumerable<BO.VolunteerFields> s_enums =
-        (Enum.GetValues(typeof(BO.VolunteerFields)) as IEnumerable<BO.VolunteerFields>)!;
+                  (Enum.GetValues(typeof(BO.VolunteerFields)) as IEnumerable<BO.VolunteerFields>)!;
 
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
-
-public class CallFieldsCollection : IEnumerable
+internal class CallFieldsCollection : IEnumerable
 {
     static readonly IEnumerable<BO.CallInListField> s_enums =
-        (Enum.GetValues(typeof(BO.CallInListField)) as IEnumerable<BO.CallInListField>)!;
+                  (Enum.GetValues(typeof(BO.CallInListField)) as IEnumerable<BO.CallInListField>)!;
 
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
@@ -74,7 +24,7 @@ public class CallFieldsCollection : IEnumerable
 public class TypeOfCallCollection : IEnumerable
 {
     static readonly IEnumerable<BO.TypeOfCall> s_enums =
-        (Enum.GetValues(typeof(BO.TypeOfCall)) as IEnumerable<BO.TypeOfCall>)!;
+                  (Enum.GetValues(typeof(BO.TypeOfCall)) as IEnumerable<BO.TypeOfCall>)!;
 
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
@@ -82,25 +32,12 @@ public class TypeOfCallCollection : IEnumerable
 public class Role : IEnumerable
 {
     static readonly IEnumerable<BO.Role> s_enums =
-        (Enum.GetValues(typeof(BO.Role)) as IEnumerable<BO.Role>)!;
-
+                  (Enum.GetValues(typeof(BO.Role)) as IEnumerable<BO.Role>)!;
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
-
 public class TypeOfDistance : IEnumerable
 {
     static readonly IEnumerable<BO.TypeOfDistance> s_enums =
-        (Enum.GetValues(typeof(BO.TypeOfDistance)) as IEnumerable<BO.TypeOfDistance>)!;
-
+                  (Enum.GetValues(typeof(BO.TypeOfDistance)) as IEnumerable<BO.TypeOfDistance>)!;
     public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
 }
-
-public class CallStatusCollection : IEnumerable
-{
-    static readonly IEnumerable<BO.CallStatus> s_enums =
-        (Enum.GetValues(typeof(BO.CallStatus)) as IEnumerable<BO.CallStatus>)!;
-
-    public IEnumerator GetEnumerator() => s_enums.GetEnumerator();
-}
-
-
--- PL/Volunteer/VolunteerWindow.xaml.cs
+++ PL/Volunteer/VolunteerWindow.xaml.cs
@@ -1,194 +1,17 @@
-﻿////using BO;
-////using System;
-////using System.Collections.Generic;
-////using System.Linq;
-////using System.Text;
-////using System.Threading.Tasks;
-////using System.Windows;
-////using System.Windows.Controls;
-////using System.Windows.Data;
-////using System.Windows.Documents;
-////using System.Windows.Input;
-////using System.Windows.Media;
-////using System.Windows.Media.Imaging;
-////using System.Windows.Shapes;
-
-////namespace PL.Volunteer
-////{
-////    /// <summary>
-////    /// Interaction logic for VolunteerWindow.xaml
-////    /// </summary>
-////    public partial class VolunteerWindow : Window
-////    {
-////        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-
-////        public static readonly DependencyProperty ButtonTextProperty =
-////       DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow), new PropertyMetadata("Add"));
-
-
-
-////        public BO.Volunteer? CurrentVolunteer
-////        {
-////            get { return (BO.Volunteer)GetValue(CurrentVolunteerProperty); }
-////            set { SetValue(CurrentVolunteerProperty, value); }
-////        }
-
-////        // Using a DependencyProperty as the backing store for CurrentVolunteer.  This enables animation, styling, binding, etc...
-////        public static readonly DependencyProperty CurrentVolunteerProperty =
-////            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
-
-
-////        public string ButtonText
-////        {
-////            get { return (string)GetValue(ButtonTextProperty); }
-////            set { SetValue(ButtonTextProperty, value); }
-////        }
-////        public BO.Role Role { get; set; } = BO.Role.STANDARD;
-
-////        public BO.TypeOfDistance TypeOfDistance { get; set; } = BO.TypeOfDistance.WALK;
-////        public void comboBoxTypeOfDistance_SelectionChanged(object sender, SelectionChangedEventArgs e)
-////              => CurrentVolunteer.TypeOfDistance = sender is ComboBox comboBox ? (BO.TypeOfDistance)comboBox.SelectedItem : BO.TypeOfDistance.WALK;
-////        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
-////        {
-////            if (sender is PasswordBox passwordBox)
-////            {
-////                CurrentVolunteer.Password = passwordBox.Password; // Update the CurrentVolunteer.Password property
-////            }
-////        }
-
-////        private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
-////        {
-////            try
-////            {
-////                if (ButtonText == "Add")
-////                {
-////                    s_bl.Volunteer.Create(CurrentVolunteer!);
-////                    MessageBox.Show("Volunteer added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-////                }
-////                else if (ButtonText == "Update")
-////                {
-////                    s_bl.Volunteer.Update(CurrentVolunteer.Id, CurrentVolunteer!);
-////                    MessageBox.Show("Volunteer updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-////                    CurrentVolunteer = s_bl.Volunteer.Read(CurrentVolunteer!.Id);
-////                }
-////                this.Close();
-////            }
-////            catch (Exception ex)
-////            {
-////                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-////            }
-////        }
-
-////        //private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
-////        //{
-////        //    if (ButtonText == "Add")
-////        //    {
-////        //        s_bl.Volunteer.Create(CurrentVolunteer);
-////        //        MessageBox.Show("Volunteer added successfully!");
-////        //    }
-////        //    else if (ButtonText == "Update")
-////        //    {
-////        //        s_bl.Volunteer.Update(CurrentVolunteer.Id,CurrentVolunteer);
-////        //        MessageBox.Show("Volunteer updated successfully!");
-////        //    }
-////        //}
-
-////        private void queryVolunteerDetails()
-////        {
-////            int id = CurrentVolunteer!.Id;
-////            CurrentVolunteer = s_bl.Volunteer.Read(id);
-////        }
-
-////        private void VolunteerObserver()
-////                      => queryVolunteerDetails();
-
-////        private void Window_Loaded(object sender, RoutedEventArgs e)
-////        {
-////            if (CurrentVolunteer!.Id != 0)
-////                s_bl.Volunteer.AddObserver(CurrentVolunteer!.Id, VolunteerObserver);
-////        }
-
-////        private void Window_Closed(object sender, EventArgs e)
-////                      => s_bl.Volunteer.RemoveObserver(CurrentVolunteer!.Id, VolunteerObserver);
-
-////        public VolunteerWindow(int Id=0)
-////        {
-////            ButtonText = Id == 0 ? "Add" : "Update";
-////            CurrentVolunteer = (Id != 0) ? s_bl.Volunteer.Read(Id)!
-////           : new BO.Volunteer(0, "", "", "", "", null, null, null, BO.Role.STANDARD, true, null, BO.TypeOfDistance.WALK, 0, 0, 0, null);
-
-////            InitializeComponent();
-////        }
-////        public void comboBoxRole_SelectionChanged(object sender, SelectionChangedEventArgs e)
-////      => CurrentVolunteer.Role = sender is ComboBox comboBox ? (BO.Role)comboBox.SelectedItem : BO.Role.STANDARD;
-
-////    }
-////}using BO;
-//using System;
-//using System.Windows;
-//using System.Windows.Controls;
-
-//namespace PL.Volunteer
-//{
-//    public partial class VolunteerWindow : Window
-//    {
-//        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-
-//        public static readonly DependencyProperty ButtonTextProperty =
-//            DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow), new PropertyMetadata("Add"));
-
-//        public static readonly DependencyProperty CurrentVolunteerProperty =
-//            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
-
-//        public BO.Volunteer? CurrentVolunteer
-//        {
-//            get { return (BO.Volunteer)GetValue(CurrentVolunteerProperty); }
-//            set { SetValue(CurrentVolunteerProperty, value); }
-//        }
-
-//        public string ButtonText
-//        {
-//            get { return (string)GetValue(ButtonTextProperty); }
-//            set { SetValue(ButtonTextProperty, value); }
-//        }
-
-//        public VolunteerWindow(int Id = 0)
-//        {
-//            ButtonText = Id == 0 ? "Add" : "Update";
-//            CurrentVolunteer = (Id != 0) ? s_bl.Volunteer.Read(Id)!
-//                : new BO.Volunteer(0, "", "", "", "", null, null, null, BO.Role.STANDARD, true, null, BO.TypeOfDistance.WALK, 0, 0, 0, null);
-
-//            InitializeComponent();
-//        }
-
-//        private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
-//        {
-//            try
-//            {
-//                if (ButtonText == "Add")
-//                {
-//                    s_bl.Volunteer.Create(CurrentVolunteer!);
-//                    MessageBox.Show("Volunteer added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-//                }
-//                else if (ButtonText == "Update")
-//                {
-//                    s_bl.Volunteer.Update(CurrentVolunteer!.Id, CurrentVolunteer!);
-//                    MessageBox.Show("Volunteer updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-//                    CurrentVolunteer = s_bl.Volunteer.Read(CurrentVolunteer!.Id);
-//                }
-//                this.Close();
-//            }
-//            catch (Exception ex)
-//            {
-//                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-//            }
-//        }
-//    }
-//}
-using BO;
+﻿using BO;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace PL.Volunteer
 {
@@ -197,42 +20,42 @@ namespace PL.Volunteer
     /// </summary>
     public partial class VolunteerWindow : Window
     {
-        // Static instance of the business logic layer
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-        // Dependency property for ButtonText
         public static readonly DependencyProperty ButtonTextProperty =
-            DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow), new PropertyMetadata("Add"));
+       DependencyProperty.Register("ButtonText", typeof(string), typeof(VolunteerWindow), new PropertyMetadata("Add"));
 
-        // Dependency property for CurrentVolunteer
-        public static readonly DependencyProperty CurrentVolunteerProperty =
-            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
 
-        // Property for CurrentVolunteer
+
         public BO.Volunteer? CurrentVolunteer
         {
             get { return (BO.Volunteer)GetValue(CurrentVolunteerProperty); }
             set { SetValue(CurrentVolunteerProperty, value); }
         }
 
-        // Property for ButtonText
+        // Using a DependencyProperty as the backing store for CurrentVolunteer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentVolunteerProperty =
+            DependencyProperty.Register("CurrentVolunteer", typeof(BO.Volunteer), typeof(VolunteerWindow), new PropertyMetadata(null));
+
+
         public string ButtonText
         {
             get { return (string)GetValue(ButtonTextProperty); }
             set { SetValue(ButtonTextProperty, value); }
         }
+        public BO.Role Role { get; set; } = BO.Role.STANDARD;
 
-        // Constructor for VolunteerWindow
-        public VolunteerWindow(int Id = 0)
+        public BO.TypeOfDistance TypeOfDistance { get; set; } = BO.TypeOfDistance.WALK;
+        public void comboBoxTypeOfDistance_SelectionChanged(object sender, SelectionChangedEventArgs e)
+              => CurrentVolunteer.TypeOfDistance = sender is ComboBox comboBox ? (BO.TypeOfDistance)comboBox.SelectedItem : BO.TypeOfDistance.WALK;
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
-            ButtonText = Id == 0 ? "Add" : "Update";
-            CurrentVolunteer = (Id != 0) ? s_bl.Volunteer.Read(Id)!
-                : new BO.Volunteer(0, "", "", "", "", null, null, null, BO.Role.STANDARD, true, null, BO.TypeOfDistance.WALK, 0, 0, 0, null);
-
-            InitializeComponent();
+            if (sender is PasswordBox passwordBox)
+            {
+                CurrentVolunteer.Password = passwordBox.Password; // Update the CurrentVolunteer.Password property
+            }
         }
 
-        // Event handler for Add/Update button click
         private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -244,7 +67,7 @@ namespace PL.Volunteer
                 }
                 else if (ButtonText == "Update")
                 {
-                    s_bl.Volunteer.Update(CurrentVolunteer!.Id, CurrentVolunteer!);
+                    s_bl.Volunteer.Update(CurrentVolunteer.Id, CurrentVolunteer!);
                     MessageBox.Show("Volunteer updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
                     CurrentVolunteer = s_bl.Volunteer.Read(CurrentVolunteer!.Id);
                 }
@@ -255,5 +78,49 @@ namespace PL.Volunteer
                 MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        //private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (ButtonText == "Add")
+        //    {
+        //        s_bl.Volunteer.Create(CurrentVolunteer);
+        //        MessageBox.Show("Volunteer added successfully!");
+        //    }
+        //    else if (ButtonText == "Update")
+        //    {
+        //        s_bl.Volunteer.Update(CurrentVolunteer.Id,CurrentVolunteer);
+        //        MessageBox.Show("Volunteer updated successfully!");
+        //    }
+        //}
+
+        private void queryVolunteerDetails()
+        {
+            int id = CurrentVolunteer!.Id;
+            CurrentVolunteer = s_bl.Volunteer.Read(id);
+        }
+
+        private void VolunteerObserver()
+                      => queryVolunteerDetails();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (CurrentVolunteer!.Id != 0)
+                s_bl.Volunteer.AddObserver(CurrentVolunteer!.Id, VolunteerObserver);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+                      => s_bl.Volunteer.RemoveObserver(CurrentVolunteer!.Id, VolunteerObserver);
+
+        public VolunteerWindow(int Id = 0)
+        {
+            ButtonText = Id == 0 ? "Add" : "Update";
+            CurrentVolunteer = (Id != 0) ? s_bl.Volunteer.Read(Id)!
+           : new BO.Volunteer(0, "", "", "", "", null, null, null, BO.Role.STANDARD, true, null, BO.TypeOfDistance.WALK, 0, 0, 0, null);
+
+            InitializeComponent();
+        }
+        public void comboBoxRole_SelectionChanged(object sender, SelectionChangedEventArgs e)
+      => CurrentVolunteer.Role = sender is ComboBox comboBox ? (BO.Role)comboBox.SelectedItem : BO.Role.STANDARD;
+
     }
 }
