ca1ab07dcb2fd93ca657c3dd40eb6573a1742921
BatyaBracha <Hand4d2@mbjcomp.org.il>
2025-07-02 05:33:44 +0300

    Add DateTimeToStringConverter for WPF data binding
    Introduced a new `DateTimeToStringConverter` class in the `PL.Call`
    namespace, implementing the `IValueConverter` interface for WPF.
    
    - `Convert` method: Converts `DateTime` to ISO 8601 string ("o" format).
    - `ConvertBack` method: Parses ISO 8601 string to `DateTime`, returning
      `DependencyProperty.UnsetValue` on failure.
    - Added necessary namespaces: `System`, `System.Globalization`,
      `System.Windows.Data`, and `System.Windows`.

---
+++ PL/Call/DateTimeToStringConverter .cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+namespace PL.Call
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dt)
+                return dt.ToString("o"); // פורמט ISO 8601 עם זמן ואזור
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DateTime.TryParse((string)value, null, DateTimeStyles.RoundtripKind, out var result))
+                return result;
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
