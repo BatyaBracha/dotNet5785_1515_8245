12e788dbfa027dc1d3f1986c74172e0cac10e52f
Batsheva-Malka <hand4d30@mbjcomp.org.il>
2025-07-02 06:29:57 +0300

    commiting..

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -11,9 +11,7 @@ public static class Initialization
     private static readonly Random s_rand = new();
     private static void CreateVolunteer()
     {
-        const int MIN_ID = 100000000;
-        const int MAX_ID = 999999999;
-        string[] names = { "Tzipy", "Chani", "Yehudit", "Shira", "Moishy", "Efraim", "Shloimy", "Yosi", "Yoni", "Avi", "Chaim", "Ester", "Rachel", "Shani", "Michali" };
+        int[] validIsraeliIds =  { 218853810, 236195475, 555762350, 977675206, 412935462, 741382543, 750319766, 414983635, 389204397, 905626214, 892208588, 816012173, 896314481 }; string[] names = { "Tzipy", "Chani", "Yehudit", "Shira", "Moishy", "Efraim", "Shloimy", "Yosi", "Yoni", "Avi", "Chaim", "Ester", "Rachel", "Shani", "Michali" };
         string[] phones = { "0527614421", "0527614422", "0527614423", "0527614424", "0527614425", "0527614426", "0527614427", "0527614428", "0527614429", "0527614430", "0527614431", "0527614432", "0527614433", "0527614434", "0527614435" };
         string[] addresses = { "Malchei Israel 12", "Minchat Itzchak 23", "Hashofar 3", "Hare'em 14", "Eben Ezra 6", "HaTzvi 4", "Hakfir 5", "Hadishon 6", "Ha'ari 13", "Hatan 17", "Tchelet Mordechei 18", "Hamarkalim 4", "Rabi Akiva 15", "Hazon Ish 53", "Harav Shach 3" };
         (double Latitude, double Longitude)[] coordinates = {(31.771959, 35.217018 ), (  31.775469, 35.229568 ),  (  31.774578, 35.207031 ),
@@ -22,11 +20,11 @@ public static class Initialization
   (31.781869,  35.236349 ),(  31.799467,  35.240458 ), (  31.788469,  35.235679 )};
         string[] passwords = { "0ZRhtg@fX8", "Yyxk%zHBBv", "xaCVl@@YVk", "op*pix&iZ&", "(zdMnv9K26", "A7bR#ZX^^2", "pnK6U31HCR", "2tvFx(*92!", "C0pjk1NzwI", "aAmrbepq#3", "%1IlDaLWn%", "chNF08*vWn", "qBf$@2HDyS", "ca*)&UrEgJ", "p!L2z#7h@m}" };
         int[] maxDistances = { 219, 748, 298, 767, 893, 419, 974, 908, 959, 450, 166, 978, 595, 250, 575 };
-        s_dal!.Volunteer.Create(new(s_rand.Next(MIN_ID, MAX_ID), "Mali Horowitz", "0583278625", "mh078625@gmail.com", "password", "Ha'ayalot 25", 31.8, 35.1, Role.ADMINISTRATOR, true, 250, (TypeOfDistance)s_rand.Next(0, 2)));
+        s_dal!.Volunteer.Create(new(328118245, "Mali Horowitz", "0583278625", "mh078625@gmail.com", "password", "Ha'ayalot 25", 31.8, 35.1, Role.ADMINISTRATOR, true, 250, (TypeOfDistance)s_rand.Next(0, 2)));
 
         for (int i = 0; i < 15; i++)
         {
-            s_dal!.Volunteer.Create(new(s_rand.Next(MIN_ID, MAX_ID), names[i], phones[i], $"{names[i]}{phones[i]}@gmail.com", passwords[i], addresses[i], coordinates[i].Latitude, coordinates[i].Longitude, Role.STANDARD, true, maxDistances[i], (TypeOfDistance)s_rand.Next(0, 2)));
+            s_dal!.Volunteer.Create(new(validIsraeliIds[i], names[i], phones[i], $"{names[i]}{phones[i]}@gmail.com", passwords[i], addresses[i], coordinates[i].Latitude, coordinates[i].Longitude, Role.STANDARD, true, maxDistances[i], (TypeOfDistance)s_rand.Next(0, 2)));
         }
 
     }
